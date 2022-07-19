8beee6099dc7299197744a463ad0eb2365c2eb50
Anh Cuong <tranacuong102@gmail.com>
2022-07-19 18:24:41 +0700

    REN-6 Banner

--- src/components/home/Banner/Banner.styled.ts
+++ src/components/home/Banner/Banner.styled.ts
@@ -1,3 +1,151 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.section`
+  position: relative;
+  margin-top: 20px;
+`;
+
+export const Industry = styled.div`
+  position: absolute;
+  width: 100%;
+  background: url(https://meta.tourzy.vn/public/frontend/showroom/images/home-bg.png)
+    no-repeat;
+  background-size: cover;
+  top: -30vh;
+  height: calc(100% + 200px);
+`;
+
+export const SecWording = styled.div`
+  position: relative;
+  text-align: center;
+
+  @media screen and (min-width: 992px) {
+    max-width: 970px;
+    margin: auto;
+  }
+  @media screen and (min-width: 768px) {
+    max-width: 750px;
+    margin: auto;
+  }
+`;
+
+export const Title = styled.h2`
+  font-size: 2.2rem;
+  font-weight: 600;
+  margin-top: 2rem;
+  color: #fff;
+`;
+
+export const Desc = styled.p`
+  color: #fff;
+  font-size: 1.2rem;
+  width: 100%;
+  margin: 0 auto 2rem;
+`;
+
+export const CardBox = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  width: 100%;
+  margin: auto;
+  grid-gap: 50px;
+
+  @media only screen and (min-width: 1200px) {
+    width: 1170px;
+  }
+
+  @media screen and (min-width: 992px) {
+    max-width: 970px;
+  }
+  @media screen and (max-width: 768px) {
+    display: flex;
+    max-width: 750px;
+    flex-direction: column;
+  }
+`;
+
+export const IndustryCard = styled.div`
+  text-align: center;
+  position: relative;
+
+  @media screen and (max-width: 1200px) {
+    flex-basis: 33%;
+  }
+`;
+
+export const WordingCard = styled.div`
+  background: #fff;
+  margin-top: 160px;
+`;
+
+export const Wording = styled.div`
+  padding: 1.5rem 0.5rem 0;
+`;
+
+export const WordingTitle = styled.h4`
+  color: #333;
+  font-weight: 400;
+  font-size: 1.5rem;
+  line-height: 1.2;
+  padding-bottom: 0;
+  margin-bottom: 0;
+`;
+
+export const WordingDesc = styled.p`
+  color: #333;
+  font-size: 1rem;
+  padding: 0 1rem;
+  margin-top: 2rem;
+  height: 158px;
+`;
+
+export const RectanBtn = styled.a`
+  margin-bottom: 1rem;
+  display: inline-block;
+  background: #000;
+  color: #fff;
+  padding: 0.8rem 2.5rem;
+  cursor: pointer;
+`;
+
+export const Img = styled.img`
+  object-fit: cover;
+  position: absolute;
+  width: 100%;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 200px;
+  padding: 0 1.5rem;
+`;
+
+export const Platform = styled.div`
+  margin: 3rem auto;
+  padding-bottom: 3rem;
+  width: 80%;
+  max-width: 1170px;
+  padding: 0.5rem 2%;
+  @media screen and (min-width: 992px) {
+    height: auto;
+  }
+`;
+
+export const ClientWrap = styled.div`
+  overflow: hidden;
+`;
+
+export const Mar = styled.marquee`
+  margin-bottom: 50px;
+`;
+
+export const ClientLogos = styled.div`
+  display: inline-flex;
+  width: 1170px;
+  grid-gap: 5px;
+  gap: 35px;
+`;
+export const ClientLogo = styled.div`
+  height: 60px;
+  min-width: 140px;
+  text-align: center;
+`;
--- src/components/home/Banner/index.tsx
+++ src/components/home/Banner/index.tsx
@@ -2,7 +2,99 @@ import * as S from './Banner.styled';
 type Props = {};
 
 const Banner = (props: Props) => {
-  return <S.Container>Banner</S.Container>;
+  return (
+    <S.Container>
+      <S.Industry></S.Industry>
+      <S.SecWording>
+        <S.Title>Nâng tầm doanh nghiệp của bạn</S.Title>
+        <S.Desc style={{lineHeight: '1.5rem'}}>
+          Tạo, xây dựng, chỉnh sửa và chia sẻ những trải nghiệm ảo đáng kinh
+          ngạc. Cho phép khách hàng của bạn hình dung và khám phá những gì bạn
+          cung cấp một cách ấn tượng nhất trong môi trường ảo.
+        </S.Desc>
+      </S.SecWording>
+      <S.CardBox>
+        <S.IndustryCard>
+          <S.WordingCard>
+            <S.Wording>
+              <S.WordingTitle>
+                <span>Chụp ảnh 360</span>
+              </S.WordingTitle>
+              <S.WordingDesc>
+                Ảnh toàn cảnh 360 độ cho phép bạn thưởng thức không gian ở mọi
+                góc độ, không giới hạn góc nhìn với độ phân giải sắc nét và thực
+                hiện xử lý hậu kỳ chuyên nghiệp!
+              </S.WordingDesc>
+            </S.Wording>
+
+            <S.RectanBtn href="https://tourzy.vn">Tìm hiểu ngay</S.RectanBtn>
+          </S.WordingCard>
+          <S.Img
+            src="https://meta.tourzy.vn/public/frontend/showroom/_nuxt/img/home-1.png"
+            alt=""
+          />
+        </S.IndustryCard>
+        <S.IndustryCard>
+          <S.WordingCard>
+            <S.Wording>
+              <S.WordingTitle>
+                <span>Showroom ảo</span>
+              </S.WordingTitle>
+              <S.WordingDesc>
+                Showroom ảo là nơi mà doanh nghiệp trưng bày và giới thiệu sản
+                phẩm đến với khách hàng. Với công nghệ chúng tôi cung cấp, bạn
+                có thể tự tạo ra một showroom ảo nhanh chóng, dễ dàng mà vẫn đảm
+                bảo hấp dẫn, mới lạ cho khách tham quan.
+              </S.WordingDesc>
+            </S.Wording>
+
+            <S.RectanBtn href="https://tourzy.vn">Tìm hiểu ngay</S.RectanBtn>
+          </S.WordingCard>
+          <S.Img
+            src="https://meta.tourzy.vn/public/frontend/showroom/_nuxt/img/home-2.png"
+            alt=""
+          />
+        </S.IndustryCard>
+        <S.IndustryCard>
+          <S.WordingCard>
+            <S.Wording>
+              <S.WordingTitle>
+                <span>Triển lãm ảo</span>
+              </S.WordingTitle>
+              <S.WordingDesc>
+                Tối đa hóa giá trị triển lãm của bạn bằng cách mở rộng triển lãm
+                thực tế sang triển lãm ảo! Với mỗi lần click chuột, khách tham
+                quan sẽ được điều hướng đến bất cứ không gian triển lãm nào
+                trong chốc lát.
+              </S.WordingDesc>
+            </S.Wording>
+
+            <S.RectanBtn href="https://tourzy.vn">Tìm hiểu ngay</S.RectanBtn>
+          </S.WordingCard>
+          <S.Img
+            src="https://meta.tourzy.vn/public/frontend/showroom/_nuxt/img/home-3.png"
+            alt=""
+          />
+        </S.IndustryCard>
+      </S.CardBox>
+      <S.Platform>
+        <S.ClientWrap>
+          <S.Mar>
+            <S.ClientLogos>
+              <S.ClientLogo>
+                No Coppy
+                <img
+                  alt="techfest-247"
+                  src="https://meta.tourzy.vn/public/frontend/showroom/_nuxt/img/fpt.png"
+                />
+                No Coppy
+              </S.ClientLogo>
+            </S.ClientLogos>
+          </S.Mar>
+        </S.ClientWrap>
+      </S.Platform>
+    </S.Container>
+  );
 };
 
 export default Banner;
